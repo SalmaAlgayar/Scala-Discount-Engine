62be77a2df863fc9380d03c61dc851c8b377468e
SalmaAlgayar <salmaalgayar47@gmail.com>
2026-04-21 15:23:53 +0200

    Remove sample data from orders-db.sql
    Removed sample data insertion for orders table.

--- docker/sql/orders-db.sql
+++ docker/sql/orders-db.sql
@@ -12,25 +12,4 @@ CREATE TABLE orders (
                         channel VARCHAR(50),
                         payment_method VARCHAR(50)
 );
-
-INSERT INTO orders (
-    timestamp,
-    product_name,
-    expiry_date,
-    quantity,
-    unit_price,
-    discount,
-    final_price,
-    channel,
-    payment_method
-) VALUES (
-             '2023-04-18T18:18:40Z',
-             'Wine - White Pinot Grigio',
-             '2023-06-10',
-             6,
-             122.47,
-             50,
-             90.9,
-             'Store',
-             'Visa'
          );
